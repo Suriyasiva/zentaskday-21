b7e2850dff57e0bede30fbcbace3524957db435d
suriya <93177461+Suriyasiva@users.noreply.github.com>
2022-01-21 23:04:34 -0800

    Add files via upload

---
+++ countown.js
@@ -0,0 +1,40 @@
+var hbd=document.createElement("div");  
+hbd.style.width="50px"
+hbd.style.height="50px"
+hbd.style.color="red"
+setTimeout(() => {
+    hbd.innerText="1"
+}, 1000);
+setTimeout(() => {
+    hbd.innerText="2"
+}, 2000);
+setTimeout(() => {
+    hbd.innerText="3"
+}, 3000);
+setTimeout(() => {
+    hbd.innerText="4"
+}, 4000);
+setTimeout(() => {
+    hbd.innerText="5"
+}, 5000);
+setTimeout(() => {
+    hbd.innerText="6"
+}, 6000);
+setTimeout(() => {
+    hbd.innerText="7"
+}, 7000);
+setTimeout(() => {
+    hbd.innerText="8"
+}, 8000);
+setTimeout(() => {
+    hbd.innerText="9"
+}, 9000);
+setTimeout(() => {
+    hbd.innerText="10"
+}, 10000);
+setTimeout(() => {
+    hbd.innerText="happy birthday"
+}, 11000);
+
+
+document.body.appendChild(hbd)
